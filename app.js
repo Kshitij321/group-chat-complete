549f3b81a04d92d6de94e7a467fd5315da813b19
Kshitij321 <ks8292.johnks@gmail.com>
2025-02-19 09:43:40 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+//serve static file of this project
+//__dirname->current working directory where the app.js
+//is present and join joins it with the public folder
+//hence forming the complete path
+
+//this adds the strings in a form of string of paths
+app.use(express.static(path.join(__dirname)));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname));
+});
+
+
+
+
+const server = app.listen(PORT, () => {
+  console.log(`listening of port ${PORT}`);
+});
+
+//initilise the web socket server
+const io = require("socket.io")(server);
+//set to store the socket ids
+let s = new Set();
+//create a connection event
+//when someone connects print their id
+//that was provided to them by this websocket
+const updateclients = (socket) => {
+  //add current id to the set
+  s.add(socket.id);
+  //emit event when a client is added
+  io.emit("clients-total", s.size);
+  socket.on("disconnect", () => {
+    s.delete(socket.id);
+    //emit event when cleint is disconnected
+    io.emit("clients-total", s.size);
+    
+  });
+
+
+  socket.on("message", (data) => {
+    //sends data to all the other sockets by emitting
+    
+    socket.broadcast.emit("chat-message", data);
+  });
+
+  
+  socket.on('feedback',(data)=>{
+    socket.broadcast.emit("feedback",data);
+  })
+};
+
+io.on("connection", updateclients);
---
+++ main.js
@@ -0,0 +1,110 @@
+//initialise the socket connection
+//from frontend we are connection with outr backend websocket server
+const socket = io();
+
+
+const clienttotal = document.querySelector(".clients-total");
+socket.on("clients-total", (data) => {
+  clienttotal.innerText = `Total clients: ${data}`;
+});
+
+const messageContainer = document.querySelector(".message-container");
+const nameInput = document.querySelector(".name-input");
+const messageForm = document.querySelector(".message-form");
+const messageInput = document.querySelector(".message-input");
+const messageTone=new Audio('/iphonetone.mp3')
+
+
+const sendMessage = () => {
+  //whenever this function called then only create this json
+  //data and send it to the server
+  if(messageInput.value=='') return;
+  const data = {
+    name: nameInput.value,
+    message: messageInput.value,
+    date: new Date(),
+  };
+ 
+  socket.emit('message',data);
+  addmessagetoui(true,data);
+  messageInput.value='';
+};
+
+socket.on('chat-message',((data)=>{
+  messageTone.play();
+    addmessagetoui(false,data);
+}))
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendMessage();
+});
+
+
+const addmessagetoui=(isown,data)=>{
+  clearFeedback();
+ const element=`<li class="${isown?"message-right":"message-left"}">
+          <p class="message">
+            ${data.message}
+            <span>${data.name} | ${moment(data.date).fromNow()}</span>
+          </p>
+        </li>`
+        messageContainer.innerHTML+=element;
+        scrolltobottom();
+}
+
+
+const scrolltobottom=()=>{
+    messageContainer.scrollTo(0,messageContainer.scrollHeight);
+}
+
+
+ messageInput.addEventListener('focus',((e)=>{
+    const data={
+      feedback:`${nameInput.value} is typing...`
+    }
+    socket.emit('feedback',(data));
+ }))
+ messageInput.addEventListener('keypress',(()=>{
+  socket.emit('feedback',{
+    feedback:`${nameInput.value} is typing...`
+  })
+ }))
+ messageInput.addEventListener('blur',(()=>{
+  socket.emit('feedback',{
+    feedback:''
+  })
+ }))
+
+
+
+ //listening to the other 
+ //events emitted by the other sockets for the feedback
+ socket.on('feedback',((data)=>{
+  clearFeedback();
+  const element=`<li class="message-feedback">
+            <p class="feedback" id="feedback">
+                ${data.feedback}
+            </p>
+
+        </li>`
+        messageContainer.innerHTML+=element;
+ }))
+
+ 
+ const clearFeedback=()=>{
+  document.querySelectorAll('li.message-feedback').forEach((element)=>{
+    element.parentNode.removeChild(element);
+  })
+ }
+ const nameInputBox=document.querySelector('.name-input-box');
+ nameInput.addEventListener("mouseenter", function() {
+  nameInputBox.style.visibility = "visible";
+});
+
+// Hide the h3 element when mouse leaves the input field
+nameInput.addEventListener("mouseleave", function() {
+  nameInputBox.style.visibility = "hidden";
+})
+
+
